7d564d75e394cfd2aaf69fbcfd33df2d74990fa5
Jeong Yeon Hwang <kukuro1995@naver.com>
2025-02-17 21:46:13 +0900

    query practice

--- back/src/main/java/com/han_batang/back/repository/ChatRoomRepository.java
+++ back/src/main/java/com/han_batang/back/repository/ChatRoomRepository.java
@@ -4,12 +4,20 @@ import java.util.List;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.han_batang.back.entity.ChatRoomEntity;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, Integer>{
-    List<ChatRoomEntity> findByChatRoomName(String chatRoomName);
+    
+    @Query(value = """
+            SELECT ch
+            FROM chat_room ch
+            WHERE ch.chatRoomName IN :chatRoomName
+            """)
+    List<ChatRoomEntity> getChatRoom(@Param("chatRoomName")String chatRoomName);
     ChatRoomEntity findByChatRoomId(Integer chatRoomId);
 } 
--- back/src/main/java/com/han_batang/back/repository/UserInfoRepository.java
+++ back/src/main/java/com/han_batang/back/repository/UserInfoRepository.java
@@ -3,12 +3,21 @@ package com.han_batang.back.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.han_batang.back.entity.UserInfoEntity;
 
 @Repository
 public interface UserInfoRepository extends JpaRepository<UserInfoEntity, Integer>{
-    Optional<UserInfoEntity> findByUserEntity_UserId(String userId);
+    
+    @Query(value = """
+            SELECT ui
+            FROM user_info ui
+            JOIN ui.userEntity u
+            WHERE u.userId = :userId
+            """)
+    Optional<UserInfoEntity> getUserInfo(@Param("userId")String userId);
     Optional<UserInfoEntity> findByNickName(String nickName);
 }
