cf752a2b1d1d6aaab05e5d49700ae8fe08127048
Jeong Yeon Hwang <kukuro1995@naver.com>
2026-01-13 00:36:49 +0900

    소셜 로그인 재구성

--- front/src/views/Authentifcation/OAuth/index.tsx
+++ front/src/views/Authentifcation/OAuth/index.tsx
@@ -6,21 +6,19 @@ export default function OAuth() {
   
     const { token, expirationTime } = useParams();
     const [cookie, setCookie] = useCookies();
-    const navigate = useNavigate();
   
     useEffect(()=> {
 
         if(!token || !expirationTime) return;
 
         console.log('토큰 발급!')
-        const now = new Date().getTime()*1000;
+        const now = Date.now();
         const expires = new Date(now + Number(expirationTime));
         setCookie('accessToken', token, {expires, path: '/' });
         
-        navigate('/');
-        window.location.href = "/";
+        window.location.replace('/');
 
-    }, [token]);
+    }, [token, expirationTime]);
     return (
       <div></div>
   )
